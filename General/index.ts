2d19b4d38cf3113dd0280962087f4d4718980915
Uneven Coder <ameenshafeeq2005@gmail.com>
2021-06-29 12:47:58 +0300

    packages

--- General/index.ts
+++ General/index.ts
@@ -7,7 +7,14 @@ const getToken = function(obj:any ): string {
   const token = obj['token'];
   return token
 }
-const client = new Discord.Client();
+const client = new Discord.Client( {fetchAllMembers: false,
+  presence: {
+    status: 'online',
+    activity: {
+      name: `to Quran`,
+      type: 'LISTENING'
+    } 
+  }});
 
 let prefix:String = '<' 
 // later add an option to change the prefix in the server it is 
@@ -16,10 +23,25 @@ client.on('ready', () => {
   console.log('I am ready!');
 });
 
-client.on('message', message => {
-
-  if (message.content === 'ping') {
-   message.channel.send('pong')
+client.on('message', async message => {
+  if (message.channel.type !== "dm") {
+    if (message.author.bot == false) {
+      if (message.content.toLowerCase() == 'as') {
+        await message.channel.send('السَّلاَم عَلَيْكُمْ وَرَحْمَةُ اللهِ وَبَرَكَاتُهُ')
+      }
+      if (message.content.toLowerCase() == 'ws') {
+        await message.channel.send('وعليكم السلام ، ورحمة الله وبركاته')
+      }
+      if (message.content.toLowerCase() == 'jzk') {
+        await message.channel.send('جَزَاكَ ٱللَّٰهُ خَيْرًا‎')
+      }
+      if (message.content.toLowerCase() == 'ms') {
+        await message.channel.send('   ما شاء الله لا قوة إلا بالله')
+      }
+      if (message.content.toLowerCase() == 'wi') {
+        await message.channel.send('  وَأَنْتُمْ فَجَزَاكُمُ ٱللَّٰهُ خَيْرًا‎')
+      }
+    }
   }
 });
 
