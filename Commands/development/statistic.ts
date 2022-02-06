2de47a7c1d3fd830838bfb988c4614120cda3b7b
Adam Amer <adamamer1801@gmail.com>
2022-02-06 15:49:30 +0200

    Added more detailed devstat statistics

--- Commands/development/statistic.ts
+++ Commands/development/statistic.ts
@@ -13,12 +13,13 @@ export const devstat: inter.command = {
         const now = Date.now()
         const test = getConnection().getRepository(Schedules).createQueryBuilder("p").select("p.scheduleName").where("p.guildIdentifier = 0").getMany()
         const time = Date.now() - now;
-        e.addField("CPU Used", `CPU: ${process.cpuUsage().user}\nIPC Sent: ${process.resourceUsage().ipcSent}\nIPC Received:${process.resourceUsage().ipcReceived}`, true)
+        e.addField("Process Information", `Node.js ${process.version}\nUptime: ${process.uptime().toFixed(2)} seconds`, false)
+        e.addField("CPU Used", `CPU: ${process.cpuUsage().user}\nIPC Sent: ${process.resourceUsage().ipcSent}\nIPC Received: ${process.resourceUsage().ipcReceived}`, true)
         e.addField("Memory Used", `RSS: ${process.memoryUsage.rss() / 1000} KB\nMax RSS: ${process.resourceUsage().maxRSS}`, true)
         e.addField("Disk I/O", `Read: ${process.resourceUsage().fsRead}\nWrite: ${process.resourceUsage().fsWrite}`)
         e.addField("Database Latency", String(time) + "ms", true)
         e.addField("Bot Latency", (String(Date.now() - message.createdTimestamp)) + "ms", true)
         e.setFooter("CPU Usage is for the entire user. In a docker environment, this should be roughly what the bot takes. For a non-docker environment, please ignore it.")
-        message.reply({embeds:[e]})
+        message.reply({ embeds: [e] })
     }
 }
