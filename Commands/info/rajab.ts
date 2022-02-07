a5e3f7df152983bf7898c08b10969c3902c3b8dc
Ameen Shafeeq <ameenshafeeq2005@gmail.com>
2022-02-07 19:34:59 +0300

    rajab 2.0

--- Commands/info/rajab.ts
+++ Commands/info/rajab.ts
@@ -5,7 +5,7 @@ export async function run(interaction?: Interaction, message?: Message) {
 
 The month of Rajab is among the sacred months in Islam along with Dhul - Qi3dah, Dhul - Hijjah and Al - Muharram and the reason for these being sacred is for the following:
 
-    1.) Fighting in these months are Haram unless the enemy transgresses upon you.
+1.) Fighting in these months are Haram unless the enemy transgresses upon you.
 
 2.) Sinning in these months are worse than sinning outside of these months(as in they are still sins, but sinning in these months are worse)
 
@@ -13,7 +13,7 @@ It has been reported by scholars such as Ibn Taymiyah, Ibn al Qayyim, and sheikh
 
 Another point to add about rajab.Fasting or singling out the 27th day or night of rajab for fasting or night prayers is an Innovation “every newly invented thing is an innovation, every innovation is misguidance, every misguidance is in the hellfire” as was reported in Sunnan An - Nisa’i(1578)
 
-In conclusion, fasting during the sacred months is recommended, but singling out a certain month like the months of rajab has no evidence for it.`
+In conclusion, fasting during the sacred months is recommended, but singling out a certain month like the month of rajab has no evidence for it.`
 
     message?.channel.send(msg)
 }
